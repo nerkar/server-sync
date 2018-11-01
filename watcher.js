2743f307c10db545f454919401a961ce10b949cb
praveen <nerkarpraveen@gmail.com>
2018-11-01 23:45:47 +0530

    removed unnecessary files

--- watcher.js
+++
@@ -1,6 +0,0 @@
-var chokidar = require('chokidar');
-
-// One-liner for current directory, ignores .dotfiles
-chokidar.watch('.', { ignored: /(^|[\/\\])\../ }).on('all', (event, path) => {
-    console.log(event, path);
-});
