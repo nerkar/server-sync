96cedfb49efcad5435fa20a2a3672fe7b26d0323
nerkar <nerkarpraveen@gmail.com>
2018-11-05 16:23:57 +0530

    removed extra

--- test/watcher.js
+++
@@ -1,6 +0,0 @@
-var chokidar = require('chokidar');
-
-// One-liner for current directory, ignores .dotfiles
-chokidar.watch('.', { persistant: true }).on('all', (event, path) => {
-    console.log(event, path);
-});
